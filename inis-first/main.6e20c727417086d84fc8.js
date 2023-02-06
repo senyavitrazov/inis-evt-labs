ca36c506995b8344bfeff55fa2addf156453da03
vitrazov2 <arsenii2509@gmail.com>
2023-02-06 12:20:27 +0300

    feat: add first inis lab

---
+++ inis-first/main.6e20c727417086d84fc8.js
@@ -0,0 +1,49 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+const REPETITIONS = 2;
+
+function validationNumber(message) {
+  let value = '';
+  while (!value || !Number(value) || Number(value) < 0 || value.length > 50) {
+    value = prompt(message);
+    if(message.length < 35) {
+      message = 'Введите валидное значение. ' + message;
+    }
+  };
+  return value
+}
+
+function validation(message) {
+  let value = '';
+  while (!value || value.length > 50) {
+    value = prompt(message);
+    if(message.length < 23) {
+      message = 'Введите валидное значение. ' + message;
+    }
+  };
+  return value;
+}
+
+let message = 'Сколько фильмов вы уже посмотрели?';
+let numberOfFilms = validationNumber(message);
+
+let nameMessage = 'Один из последних просмотренных фильмов?';
+let ratingMessage = 'На сколько оцените его?';
+
+let personalMovieDB = {
+  count: null,
+  movies: {},
+};
+
+
+for (let index = 0; index < REPETITIONS; index++) {
+  let tempName = validation(nameMessage);
+  let tempRating = validationNumber(ratingMessage);
+  personalMovieDB['count'] = numberOfFilms;
+  personalMovieDB['movies'][tempName] = tempRating;
+}
+
+console.log(personalMovieDB);
+
+/******/ })()
+;
