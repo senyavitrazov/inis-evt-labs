ced3af32067048f5ea4d94f46e3d0a9723ea3685
vitrazov2 <arsenii2509@gmail.com>
2023-02-16 16:17:28 +0300

    feat: fin lab

--- src/index.js
+++ src/index.js
@@ -1,44 +1,19 @@
-const REPETITIONS = 2;
-
-function validationNumber(message) {
-  let value = '';
-  while (!value || !Number(value) || Number(value) < 0 || value.length > 50) {
-    value = prompt(message);
-    if(message.length < 35) {
-      message = 'Введите валидное значение. ' + message;
-    }
-  };
-  return value
+const personalMovieDB = {
+  privat: false,
+  movies: {
+    'The Hunt': 10,
+    'Heat': 9,
+    'RAMBO First Blood': 9,
+  }
 }
 
-function validation(message) {
-  let value = '';
-  while (!value || value.length > 50) {
-    value = prompt(message);
-    if(message.length < 23) {
-      message = 'Введите валидное значение. ' + message;
-    }
-  };
-  return value;
+const table = document.createElement('table');
+table.classList.add('table');
+table.innerHTML += "<tr><td>Фильмы</td><td>Оценка</td></tr>"
+
+for (key in personalMovieDB.movies) {
+  table.innerHTML += `<tr><td>${key}</td><td>${personalMovieDB.movies[key]}</td></tr>`
 }
 
-let message = 'Сколько фильмов вы уже посмотрели?';
-let numberOfFilms = validationNumber(message);
 
-let nameMessage = 'Один из последних просмотренных фильмов?';
-let ratingMessage = 'На сколько оцените его?';
-
-let personalMovieDB = {
-  count: null,
-  movies: {},
-};
-
-
-for (let index = 0; index < REPETITIONS; index++) {
-  let tempName = validation(nameMessage);
-  let tempRating = validationNumber(ratingMessage);
-  personalMovieDB['count'] = numberOfFilms;
-  personalMovieDB['movies'][tempName] = tempRating;
-}
-
-console.log(personalMovieDB);
+document.querySelector('body').append(table);
