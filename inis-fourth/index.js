a591b038029ff9433010c7c3963453d250c4c4a5
vitrazov2 <arsenii2509@gmail.com>
2023-03-20 11:29:08 +0300

    feat: fin lab

--- inis-fourth/index.js
+++ inis-fourth/index.js
@@ -12,12 +12,15 @@ function createCard(image, title, amount, id) {
   </div>`);
 } 
 
-const toPage = (title) => {
-  console.log(title);
-  window.history.pushState({}, '', window.location.origin + `/inis-fourth/${title}`);
-}
-
 const container = document.querySelector('.cards-container');
+
+localStorage.setItem('color', 'white');
+localStorage.setItem('side', 'front');
+
+const toPage = (title) => {
+  history.pushState({}, null, window.location.origin + `/inis-fourth/${title}`);
+  container.innerHTML = details(shirts[window.location.pathname.slice(-1)]);
+}
 
 for (let index = 0; index < Object.keys(shirts).length; index++) {
   container.innerHTML += createCard(
@@ -30,5 +33,10 @@ for (let index = 0; index < Object.keys(shirts).length; index++) {
 
 container.addEventListener('click', e => {
   const target = e.target;
-  toPage(target.id);
+  if (target.id.slice(0, -1) == 'shirt-') {
+    localStorage.setItem('shirt', JSON.stringify(shirts[target.id.slice(-1)]));
+    location.href = './details.html'
+  }
 })
+
+window.addEventListener('event', (e) => {console.log(e);})
